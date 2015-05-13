ae6f4abfca73c7240df620a616bae2165babb5fd
Jesse Clark <jjclark1982@gmail.com>
2015-05-13 16:32:00 +1000

    make flask module available to templates

--- server.py
+++ server.py
@@ -5,17 +5,18 @@ if 'DATABASE_URL' in os.environ:
     import database
 else:
     database = None
-from flask import Flask, request, session, render_template, send_from_directory, redirect
+import flask
+from flask import Flask, redirect, render_template, send_from_directory
 
 app = Flask(__name__.split('.')[0], template_folder='public')
-app.debug = ('DEBUG' in os.environ)
+app.debug = os.environ.get('DEBUG')
 
 # Catch-all route handler
 @app.route('/', defaults={'path': '/'})
 @app.route('/<path:path>')
 def serve_file(path):
     # serve a directory
-    if os.path.isdir(os.path.join('public', path)):
+    if os.path.isdir(os.path.join(app.template_folder, path)):
         if path[-1] == '/':
             return serve_file(path + 'index.html')
         else:
@@ -24,11 +25,11 @@ def serve_file(path):
     # serve a template file
     basename, extname = os.path.splitext(path)
     if extname == '.html':
-        return render_template(path, db=database, request=request, session=session)
+        return render_template(path, db=database, flask=flask)
 
     # serve a static file
     else:
-        return send_from_directory('public', path)
+        return send_from_directory(app.template_folder, path)
 
 # For running this file directly
 if __name__ == '__main__':
